cc1aa0b93dc87052b2ca56f5e05a84b8893dd0c4
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-03-11 19:39:34 -0700

    Initial Commit

---
+++ TicTacToeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TicTacToeApp
+//
+//  Created by Sothesom on 21/12/1402.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TicTacToeApp/TicTacToeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeAppApp.swift
+//  TicTacToeApp
+//
+//  Created by Sothesom on 21/12/1402.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
